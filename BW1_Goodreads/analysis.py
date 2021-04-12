e85c13af133390e2bab89f3884caf4d47abec756
Vladimir Gasanov <vladimir.gas902@gmail.com>
2021-04-12 12:00:41 +0200

    goodreads initial commit

---
+++ BW1_Goodreads/analysis.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 12 11:59:41 2021
+
+@author: vladimirgasanov
+"""
+
---
+++ BW1_Goodreads/scraper.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 12 11:56:08 2021
+
+@author: vladimirgasanov
+"""
+
